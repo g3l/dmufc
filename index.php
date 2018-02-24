bdc8d09318660910492917adc1c455f03fc00a69
Godfrey Lemi <godfreylemi@gmail.com>
2018-02-24 20:07:36 +0000

    Changed to php files

--- index.php
+++ index.php
@@ -9,14 +9,7 @@
 </head>
 <body>
 <!-- Header -->
-<div id="header">
-  <div class="shell"> 
-    <!-- Logo --> 
-    <img class="logo" src="css/images/logo.png" alt="Club Logo" />
-    <h1 class="titletext">De Montfort University Mens Football club</h1>
-    <!-- End Logo --> 
-  </div>
-</div>
+require_once 'header.php';
 <!-- End Header --> 
 <!-- Navigation -->
 <div id="navigation">
