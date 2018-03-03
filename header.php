4d8d2dd36a40179c12479b9dfc915055e36e2ec1
Godfrey Lemi <33334140+g3l@users.noreply.github.com>
2018-03-03 01:02:01 +0000

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<div id="header">
-  <div class="shell"> 
-    <!-- Logo --> 
-    <img class="logo" src="css/images/logo.png" alt="Club Logo" />
-    <h1 class="titletext">De Montfort University Mens Football club</h1>
-    <!-- End Logo --> 
-  </div>
-</div>
